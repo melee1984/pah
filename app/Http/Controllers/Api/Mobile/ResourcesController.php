dc58b9ce5ccf94c9414b2af3bc93465707c574e4
Leslie Parba <lparba@gmail.com>
2026-07-24 17:04:07 +0800

    fix function name

--- app/Http/Controllers/Api/Mobile/ResourcesController.php
+++ app/Http/Controllers/Api/Mobile/ResourcesController.php
@@ -104,7 +104,7 @@ class ResourcesController extends Controller
         ]);
     }
 
-    public function getTopPics()
+    public function getTopPicks()
     {
         $topPicks = [
             [
