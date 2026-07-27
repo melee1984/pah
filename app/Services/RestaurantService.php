89d33cdb8158e5a8d7b5291b9d68627224bda76f
Leslie Parba <lparba@gmail.com>
2026-07-27 15:08:54 +0800

    make sure that we are getting the logo and first image.

--- app/Services/RestaurantService.php
+++ app/Services/RestaurantService.php
@@ -72,18 +72,16 @@ class RestaurantService
             $hasItemImage = false;
             
             foreach($restaurant->products as $product) {
-
+                $product->getPriceDisplay();
                 // Get the image here from the product library 
                 if ($product->img!="") {
                     $imagePath = Partners::imageResizeThumb($product, $product->id);
                     $restaurant->img = $imagePath;
                     $restaurant->image_url = $imagePath;
                     $product->image_url  = $imagePath;
-             
                     $hasItemImage = true; // this will get the first image and break the loop
+                    break;
                 }
-
-                $product->getPriceDisplay();
             }
 
             
