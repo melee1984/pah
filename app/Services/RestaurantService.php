ec6913e7aa399f1ce21e1ea357455bf487538475
Leslie Parba <lparba@gmail.com>
2026-07-29 20:13:21 +0800

    logging the reuest from the data/restaurant

--- app/Services/RestaurantService.php
+++ app/Services/RestaurantService.php
@@ -14,6 +14,8 @@ class RestaurantService
     {
        $data = array();
 
+       \Log::info(['message' => 'Fetching restaurants for the mobile app', 'request' => $request->all()]);
+       
         $session_id = $request->session_id ?? $request->session()->getId();
         $cart = Cart::whereSessionId($session_id)->first();
 
