21f8886361e1c138036c39946961285970532a12
Leslie Parba <lparba@gmail.com>
2026-07-30 11:05:16 +0800

    cart information

--- app/Http/Controllers/Api/Mobile/CartController.php
+++ app/Http/Controllers/Api/Mobile/CartController.php
@@ -221,16 +221,7 @@ class CartController extends Controller
                     $cart->user_long = $request->input('longitude');
                     $cart->user_lat = $request->input('latitude');
                 }
-
-                Log::info('CartController@addCart: Saving cart', [
-                    'session_id' => $cart->session_id,
-                    'user_id' => $user?->id,
-                    'partner_id' => $partnerId,
-                    'user_long' => $cart->user_long,
-                    'user_lat' => $cart->user_lat,
-                ]);
-
-                $cart->ip_address = $request->ip();
+              $cart->ip_address = $request->ip();
                 $cart->partner_id = $partnerId;
                 // makuha na man kung kinsa ang iyaha location but possible na multiple location sya. need to recheck that. 
                 $cart->partner_location_address_id = $cart->partner->location->id ?? null; 
@@ -244,6 +235,14 @@ class CartController extends Controller
                 }
 
                 $cart->save();
+
+                Log::info('CartController@addCart: Saving cart', [
+                    'session_id' => $cart->session_id,
+                    'user_id' => $user?->id,
+                    'partner_id' => $partnerId,
+                    'user_long' => $cart->user_long,
+                    'user_lat' => $cart->user_lat,
+                ]);
 
                 $serializedVariants = serialize($variantContent);
                 $cartItemAttributes = [
