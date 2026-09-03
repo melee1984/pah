7c2baf9c573eeaffde800dd246dccb6616ea874c
Leslie Parba <lparba@gmail.com>
2026-09-03 15:27:06 +0800

    update n alnag nato ni status

--- app/LibraryStatus.php
+++ app/LibraryStatus.php
@@ -10,9 +10,10 @@ class LibraryStatus extends Model
     public const STATUS_ORDER_ACCEPTED = 2;
     public const STATUS_PROCESSING = 3;
     public const STATUS_READY_FOR_PICKUP = 4;
-    public const STATUS_RIDER_PICKED_UP = 5; // it should have a combination here PICKUP and BOOKING_STATUS_ON_WAY 
-    public const STATUS_DELIVERED = 6;
-    public const STATUS_CANCELLED = 7;
+    public const STATUS_RIDER_ON_THE_WAY_TO_CUSTOMER = 5; // it should have a combination here PICKUP and BOOKING_STATUS_ON_WAY
+    public const STATUS_ARRIVAL_AT_CUSTOMER = 6; // it should have a combination here PICKUP and BOOKING_STATUS_ON_WAY
+    public const STATUS_DELIVERED = 7;
+    public const STATUS_CANCELLED = 8;
 
     protected $table = 'library_status';
 	protected $fillable = array('title');
