bc5515fa96029e9e75c1f846282dcc6fb5c21f82
Leslie Parba <lparba@gmail.com>
2026-09-10 10:33:53 +0800

    updated

--- config/agent.php
+++ config/agent.php
@@ -4,4 +4,5 @@ return [
     'default_commission_percentage' => (float) env('AGENT_COMMISSION_PERCENTAGE', 30),
     'pahatud_commission_percentage' => (float) env('PAHATUD_COMMISSION_PERCENTAGE', 15),
     'restaurant_invitation_expire_hours' => (int) env('RESTAURANT_INVITATION_EXPIRE_HOURS', 72),
+    'default_commission_addup' => (bool) env('PAHATUD_COMMISSION_ADDUP', true),
 ];
